821444754710626ef74ba84bf645ca5d494a0633
Justin Xiao <jx16dev@gmail.com>
2023-10-26 17:00:09 -0700

    Implement naive http server with /healthz and /err endpoints.

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	chi "github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello world")
 
 	godotenv.Load()
 
@@ -18,5 +19,34 @@ func main() {
 		log.Fatal("PORT is not found in the environment")
 	}
 
-	fmt.Println("PORT:", portString)
+	// fmt.Println("PORT:", portString)
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
