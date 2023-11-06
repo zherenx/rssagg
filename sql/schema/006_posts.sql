1c3544f2492f4dd0fd3c7c2c9914919b91be5316
Justin Xiao <jx16dev@gmail.com>
2023-11-05 18:17:47 -0800

    Fix bug in posts table migration query.

--- sql/schema/006_posts.sql
+++ sql/schema/006_posts.sql
@@ -1,11 +1,11 @@
 -- +goose Up
 CREATE TABLE posts (
     id UUID PRIMARY KEY,
-    created_at TIME NOT NULL,
-    updated_at TIME NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     title TEXT NOT NULL,
     description TEXT,
-    published_at TIME NOT NULL,
+    published_at TIMESTAMP NOT NULL,
     url TEXT NOT NULL UNIQUE,
     feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE
 );
